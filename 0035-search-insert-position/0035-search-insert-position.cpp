2b7cdd8d2969f20fa76131db32a209fe0cf226c8
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-04-05 19:09:28 +0530

    Time: 0 ms (100%), Space: 13.8 MB (42.13%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       auto ix = lower_bound(nums.begin(), nums.end(), target);
+    
+       return ix - nums.begin();
+    }
+};
