8dca91fde676cf5b700f1eaf776fb094c23603de
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-04-15 23:55:26 +0530

    Time: 170 ms (5.12%), Space: 24 MB (19.14%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+        int i=0;
+        while(i <  nums.size()){
+            if(nums[i] == 0){
+                nums.erase(nums.begin() + i);
+                count++;
+            }else{ i++;}
+        }
+        while(count>0){
+            nums.push_back(0);
+            count--;
+        }
+        cout<< "[";
+        for(int e : nums){
+            cout << e << ",";
+        }
+        cout<< "]";
+    }
+    
+};
