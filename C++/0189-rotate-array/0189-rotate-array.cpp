3ab23e5538cf967540c56e6cc1284fbc7ee8f675
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-06-22 17:17:18 +0530

    Time: 233 ms (1.81%), Space: 29.6 MB (67.51%) - LeetHub

---
+++ C++/0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+       
+       if(k> nums.size()){ k = k% nums.size();}
+
+        reverse(nums, 0, nums.size()-1-k);
+        reverse(nums, nums.size()-k, nums.size()-1);
+        reverse(nums, 0, nums.size()-1);
+
+        cout << "[";
+        for(int ele: nums){
+            cout<< ele << ",";
+        }
+        cout << "]";
+    }
+
+    void reverse(vector<int>& nums,int l, int r){
+        while(l<=r){
+            swap(nums[l], nums[r]);
+            l++;
+            r--;
+        }
+    }
+};
+
