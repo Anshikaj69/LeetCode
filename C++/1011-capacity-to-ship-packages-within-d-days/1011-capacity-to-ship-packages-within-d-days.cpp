df9bce3cd54ff404c9f92eb0035d70a1287b25a3
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-06-28 20:50:47 +0530

    Time: 11 ms (66.21%), Space: 34.9 MB (88.25%) - LeetHub

---
+++ C++/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int l = *max_element(weights.begin(), weights.end()) ;
+        int r = accumulate(weights.begin(), weights.end(),0);
+
+        int mid, sum=0, total_days, min_w = 1000000000;
+        cout<< l;
+        cout<<r;
+
+        while(l<=r){
+            mid = (l + r)/2;
+            total_days = 0;
+            sum =0;
+            for(int i=0; i<weights.size(); i++){
+                sum = sum+ weights[i];
+                if(sum > mid){
+                    sum = weights[i];
+                    total_days += 1;
+                }
+            }
+
+            if(sum <= mid){total_days++;}
+
+            if(total_days <= days){
+                r=mid-1;
+                min_w=min(min_w, mid);
+            }else{
+                l=mid+1;
+            }
+        }
+
+        return min_w;
+    }
+};
