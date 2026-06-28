f8c438fe9d04fe8dd4064121a41fc3a13cb0820a
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-06-28 20:53:38 +0530

    Time: 10 ms (71.64%), Space: 35.1 MB (19.23%) - LeetHub

--- C++/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ C++/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -5,25 +5,23 @@ public:
         int l = *max_element(weights.begin(), weights.end()) ;
         int r = accumulate(weights.begin(), weights.end(),0);
 
-        int mid, sum=0, total_days, min_w = 1000000000;
-        cout<< l;
-        cout<<r;
+        int mid, sum=0, total_days, min_w=1000000000;
 
         while(l<=r){
-            mid = (l + r)/2;
-            total_days = 0;
-            sum =0;
+            mid=(l + r)/2;
+            total_days=0;
+            sum=0;
             for(int i=0; i<weights.size(); i++){
-                sum = sum+ weights[i];
-                if(sum > mid){
-                    sum = weights[i];
-                    total_days += 1;
+                sum = sum+weights[i];
+                if(sum>mid){
+                    sum=weights[i];
+                    total_days+=1;
                 }
             }
 
-            if(sum <= mid){total_days++;}
+            if(sum<=mid){total_days++;}
 
-            if(total_days <= days){
+            if(total_days<=days){
                 r=mid-1;
                 min_w=min(min_w, mid);
             }else{
