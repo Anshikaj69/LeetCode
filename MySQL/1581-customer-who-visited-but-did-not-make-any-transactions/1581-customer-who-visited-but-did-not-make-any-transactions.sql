7ff027bfc8835035dabf3462834eb135a417b825
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-07-11 02:14:22 +0530

    Time: 1300 ms (88.23%), Space: 0B (100%) - LeetHub

--- MySQL/1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ MySQL/1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,9 +1,18 @@
 # Write your MySQL query statement below
+#subquery use, but for every entry in visits, whole transactions table will be iterated
+-- Select customer_id, Count(customer_id) as count_no_trans
+-- from Visits as vs
+-- Where vs.visit_id Not in (
+--     Select visit_id from Transactions
+-- )
+-- Group by customer_id
+-- ;
 
-Select customer_id, Count(customer_id) as count_no_trans
-from Visits as vs
-Where vs.visit_id Not in (
-    Select visit_id from Transactions
-)
-Group by customer_id
-;
+#join
+
+Select v.customer_id, Count(v.customer_id) as count_no_trans
+from Visits as v
+left join Transactions t
+     ON v.visit_id = t.visit_id
+where t.visit_id is null
+group by v.customer_id;
