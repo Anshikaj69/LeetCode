ca5c3809d4ed528ce233cfdbf6f8321e268fe9fe
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-07-23 00:02:16 +0530

    Time: 786 ms (5.09%), Space: 94.3 MB (5.45%) - LeetHub

---
+++ C++/0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,37 @@
+class StockSpanner {
+
+public:
+
+    stack<int> s;
+    vector<int> span;
+    StockSpanner() {
+       
+    }
+    
+    int next(int price) {
+        
+        span.push_back(price);
+        int i = span.size()-1;
+        int ans = 1;
+
+        while(!s.empty() && span[s.top()] <= price){
+            cout<< "pooped on i - " << i << "value : " << span[s.top()]<<endl;
+            s.pop();
+        }
+
+        if(!s.empty()){
+        ans =  i - s.top();
+        }else{
+            ans = i+1;
+        }
+        s.push(i);
+
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
