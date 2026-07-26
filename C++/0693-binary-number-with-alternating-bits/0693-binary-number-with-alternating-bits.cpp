38b78f59531f5c1b5c657e2b622f814c72d33d65
Anshika jaiswal <94690313+Anshikaj69@users.noreply.github.com>
2026-07-26 22:50:50 +0530

    Time: 0 ms (100%), Space: 7.7 MB (92.75%) - LeetHub

--- C++/0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ C++/0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool hasAlternatingBits(int n) {
         
-
+        // can also use bit manipulation i.e if n = (n | 1) to check if lsb is set
         int flag = (n%2==0)?  1: 0 ;
 
         while(n > 0){
-            cout<< "flag: " << flag << "num : " << n;
+
             if(n%2 == flag){
                 return false;
             }
